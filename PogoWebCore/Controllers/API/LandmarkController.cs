9ea7a0bc76100c4d25e48e27b18dff19bf2bb0cd
Mark Jansen <markwilemail@gmail.com>
2020-12-17 15:39:14 +0100

    implemented webAPIs LandmarkTypesController and LandmarksController

---
+++ PogoWebCore/Controllers/API/LandmarkController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PogoWebCore;
+using PogoWebCore.Models;
+
+namespace PogoWebCore.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LandmarksController : ControllerBase
+    {
+        private readonly PogoContext _context;
+
+        public LandmarksController(PogoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/LandmarkTypes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Landmark>>> 
+            GetLandmarks([FromQuery]double? lon, [FromQuery] double? lat)
+        {
+            return await _context.Landmarks.ToListAsync();
+        }
+    }
+}
---
+++ PogoWebCore/Controllers/API/LandmarkTypesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PogoWebCore;
+using PogoWebCore.Models;
+
+namespace PogoWebCore.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LandmarkTypesController : ControllerBase
+    {
+        private readonly PogoContext _context;
+
+        public LandmarkTypesController(PogoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/LandmarkTypes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LandmarkType>>> GetLandmarkTypes()
+        {
+            return await _context.LandmarkTypes.ToListAsync();
+        }
+    }
+}
