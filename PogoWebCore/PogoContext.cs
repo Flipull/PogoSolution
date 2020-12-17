bffd0fadc46df744c0f7027743110bc941349f5b
Mark Jansen <markwilemail@gmail.com>
2020-12-17 13:51:40 +0100

    set up Entity Framework - Server Configuration, Entity Configuration, PogoDbContext

---
+++ PogoWebCore/PogoContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PogoWebCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PogoWebCore
+{
+    public class PogoContext: DbContext
+    {
+        public DbSet<LandmarkType> LandmarkTypes;
+        public DbSet<Landmark> Landmarks;
+
+        public PogoContext(DbContextOptions<PogoContext> options) : base(options)
+        { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LandmarkType>();
+            modelBuilder.Entity<Landmark>()
+                .HasOne<LandmarkType>()
+                .WithMany().HasForeignKey(l => l.TypeId)
+                .IsRequired();
+            /*
+            modelBuilder.Entity<LandmarkType>().HasData(
+                new LandmarkType { Id =,Name = "", ImageFileName="" }
+                );
+            modelBuilder.Entity<Landmark>().HasData(
+                new Landmark { Id = , Lattitude = , Longitude = , Name = "", TypeId = }
+                );
+            */
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
