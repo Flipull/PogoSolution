15180e397eb87b831836e115cd9c3558e49bd22e
Mark Jansen <markwilemail@gmail.com>
2020-12-17 12:40:17 +0100

    Add project files.

---
+++ PogoWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PogoWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
