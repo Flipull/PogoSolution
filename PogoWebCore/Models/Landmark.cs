862a76776625753453c4f7292f4aa20a379769cc
Mark Jansen <markwilemail@gmail.com>
2020-12-17 13:09:10 +0100

    Added both datatypes

---
+++ PogoWebCore/Models/Landmark.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PogoWebCore.Models
+{
+    public class Landmark
+    {
+        public int Id { get; set; }
+        public int TypeId { get; set; }
+        public string Name { get; set; }
+        public double Longitude { get; set; }
+        public double Lattitude { get; set; }
+    }
+}
---
+++ PogoWebCore/Models/LandmarkType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PogoWebCore.Models
+{
+    public class LandmarkType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageFileName { get; set; }
+    }
+}
